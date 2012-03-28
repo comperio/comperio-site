afa8ac48c2e5c4f5bf02e3dbb0c917b0f8f1c9df
Giulio Bonanome <gbonanome@gmail.com>
2012-03-28 09:48:42 +0200

    ahem...remove password

--- mysite/_config.php
+++ mysite/_config.php
@@ -7,9 +7,9 @@ global $databaseConfig;
 $databaseConfig = array(
 	"type" => 'MySQLDatabase',
 	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'uelso4eg',
-	"database" => 'SS_comperio',
+	"username" => '',
+	"password" => '',
+	"database" => '',
 	"path" => '',
 );
 
@@ -17,7 +17,7 @@ MySQLDatabase::set_connection_charset('utf8');
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.org/themes/
-SSViewer::set_theme('blackcandy');
+SSViewer::set_theme('comperio')
 
 // Set the site locale
 i18n::set_locale('en_US');
